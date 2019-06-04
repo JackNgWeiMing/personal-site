1d7ee37f18146a1ac7125581bb86548a308d754d
JackJackNg <jackng9995@gmail.com>
2019-06-04 15:43:25 +0800

    add ocean typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,11 @@ module.exports = {
     },
     'gatsby-transformer-remark',
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
   ],
 }
