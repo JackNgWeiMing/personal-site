8693ad5faf351c356862d2388daedc84527d616f
JackJackNg <jackng9995@gmail.com>
2019-06-19 09:44:46 +0800

    unactivate typography js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,17 @@
-import Typography from 'typography'
-import oceanTheme from 'typography-theme-ocean-beach'
+// import Typography from 'typography'
+// import oceanTheme from 'typography-theme-ocean-beach'
 
-oceanTheme.overrideThemeStyles = ({ rhythm }, options) => ({
-  a: {
-    textShadow: 'none', // eslint-disable-line
-    textDecoration: 'underlined',
-  },
-  'h2,h3': {
-    marginBottom: rhythm(1 / 2),
-    marginTop: rhythm(2),
-  },
-})
+// oceanTheme.overrideThemeStyles = ({ rhythm }, options) => ({
+//   a: {
+//     textShadow: 'none', // eslint-disable-line
+//     textDecoration: 'underlined',
+//   },
+//   'h2,h3': {
+//     marginBottom: rhythm(1 / 2),
+//     marginTop: rhythm(2),
+//   },
+// })
 
-const typography = new Typography(oceanTheme)
+// const typography = new Typography(oceanTheme)
 
-export default typography
+// export default typography
