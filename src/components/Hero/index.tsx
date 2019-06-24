f803f94403974bcee1e4dde5332161ea3a34df21
JackJackNg <jackng9995@gmail.com>
2019-06-24 09:52:10 +0800

    fade in effect

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,23 +1,45 @@
 import * as React from 'react'
 import Styles from '../../styles/main.module.scss'
+import { FadeIn } from '../../components'
 
 interface Props {}
 
 const Hero: React.SFC<Props> = props => {
+  const [isMounted, setMounted] = React.useState(false)
+  React.useEffect(() => {
+    const cancenlListner = setTimeout(() => {
+      setMounted(true)
+    }, 1000)
+
+    return () => clearTimeout(cancenlListner)
+  })
+
+  const one = (
+    <span>
+      <strong className={Styles.strong}>Hi my name is</strong>
+    </span>
+  )
+
+  const two = <h1>Jack Ng</h1>
+  const three = <h2>React & React Native Developer</h2>
+  const four = (
+    <p>
+      I'm a software web developer Singapore speicalized in front end
+      development in Singapore. <br /> React, React Native, Typescript are my
+      best friends. I work with them to all the time.
+    </p>
+  )
+
+  const items = [one, two, three, four]
   return (
     <section id="about" className={Styles.hero}>
-      <div>
-        <span>
-          <strong className={Styles.strong}>Hi my name is</strong>
-        </span>
-        <h1>Jack Ng</h1>
-        <h2>React & React Native Developer</h2>
-        <p>
-          I'm a software web developer Singapore speicalized in front end
-          development in Singapore. <br /> React, React Native, Typescript are
-          my best friends. I work with them to all the time.
-        </p>
-      </div>
+      {items.map((v, i) => {
+        return (
+          <FadeIn appear in={isMounted} timeout={(i + 1) * 50}>
+            {v}
+          </FadeIn>
+        )
+      })}
     </section>
   )
 }
