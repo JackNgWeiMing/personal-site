b1e25c9eb9e6e17bf0a197017b120f6731b54965
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-25 06:55:22 +0000

    Deploy JackJackNg/my-amazing-website to github.com/JackJackNg/my-amazing-website.git:gh-pages

--- src/components/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as Hero } from './Hero'
-export { default as Experience } from './Experience'
-export { default as Layout } from './layout'
-export { default as Skill } from './Skill'
-export { default as FadeIn } from './FadeIn'
-export { default as ScrollReveal } from './ScrollReveal'
--- src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export type MarkdownNode = {
-  node: {
-    id: string
-    frontmatter?: any
-    html?: string
-  }
-}
