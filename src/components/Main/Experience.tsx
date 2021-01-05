dc2ca5abd1a2936031a02fcc7f0892c162622e02
jack <jackng9995@gmail.com>
2021-01-05 11:08:20 +0800

    mobile responsive

--- src/components/Main/Experience.tsx
+++ src/components/Main/Experience.tsx
@@ -30,7 +30,6 @@ const EachExperience: React.FunctionComponent<EachExperienceProps> = function Ea
         <Company>{data.frontmatter.company}</Company>
       </Frontmatter>
       <h2>{data.frontmatter.title}</h2>
-
       <Paragraph
         dangerouslySetInnerHTML={{
           // why wrap additional using <div></div>
@@ -44,12 +43,14 @@ const EachExperience: React.FunctionComponent<EachExperienceProps> = function Ea
 };
 
 const EachExperienceContainer = styled.div`
-  padding: 20px 0;
+  padding-top: 20px;
 
   ul.job-desc {
+    margin-top: 0;
     padding-left: 0;
+
     li {
-      padding-top: 10px;
+      padding-bottom: 15px;
       list-style-type: none;
     }
   }
@@ -63,24 +64,33 @@ const EachExperienceContainer = styled.div`
       padding: 5px 10px;
       display: inline-block;
       list-style: circle;
+      margin-top: 5px;
     }
   }
 `;
 
-const Frontmatter = styled.p``;
+const Frontmatter = styled.p`
+  margin-bottom: 0;
+`;
 
 const Paragraph = styled.p`
   font-size: 0.9em;
+  line-height: 1.5em;
 `;
 
 const StartDate = styled.span`
   font-weight: bolder;
   font-size: 0.9em;
+  padding-right: 20px;
 `;
 
-const Company = styled.span`
+const Company = styled.p`
   font-size: 0.9em;
   font-weight: bolder;
-  padding-left: 20px;
   color: #f5ab62;
+  display: block;
+
+  @media only screen and (min-width: 768px) {
+    display: inline-block;
+  }
 `;
